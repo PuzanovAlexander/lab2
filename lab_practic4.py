6d42661d0f482469a0c389a70cc0732a6fd92006
PuzanovAlexander <leifon2011@gmail.com>
2023-02-26 20:03:29 +0300

    цветовой микшер

--- lab_practic4.py
+++ lab_practic4.py
@@ -1,23 +1,10 @@
-acceptedColors = ["красный", "желтый", "синий"]
-purple = ["красный", "синий"]
-orange = ["красный", "желтый"]
-green = ["желтый", "синий"]
-
-firstColor = input('Введите первый цвет: ').lower() 
-secondColor = input('Введите второй цвет: ').lower() 
-
-if firstColor in acceptedColors and secondColor in acceptedColors:
-        if firstColor == secondColor:
-            print('Введены одинаковые цвета')
-            
-
-        if firstColor in purple and secondColor in purple:
-            print('Фиолетовый')
-            
-        if firstColor in orange and secondColor in orange:
-            print('Оранжевый')
-            
-        if firstColor in green and secondColor in green:
-            print('Зеленый')
-        else:    
-            print('Введено неверное название цвета')
+a = input("Введите первый цвет:") 
+b = input("Введите второй цвет:")
+if (a == "красный" or b == "красный") and (a == "синий" or b == "синий"):
+    print("фиолетовый")
+elif (a == "красный" or b == "красный") and (a == "жёлтый" or b == "жёлтый"):
+    print("оранжевый")
+elif (a == "синий" or b == "синий") and (a == "жёлтый" or b == "жёлтый"):
+    print("зелёный")
+elif a == b and (a == "красный" or a == "синий" or a == "жёлтый"):
+    print("Ошибка цвета")
