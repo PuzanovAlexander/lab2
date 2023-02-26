b5b5f19bb77868aefbec741b9c5ba9f1adaad936
PuzanovAlexander <leifon2011@gmail.com>
2023-02-26 19:38:55 +0300

    задание_5

--- lab_practic5.py
+++ lab_practic5.py
@@ -2,7 +2,7 @@ count = int(input("Введите количество слов: "))
 line = []
 n = 0
 
-while i < count :
+while n < count :
     word = input("Введите слово " + str(n + 1) + ': ')
     line.append(word)
     n += 1
