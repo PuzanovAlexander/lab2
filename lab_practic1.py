27fb9a5537eae8abb9f518b62c0b43d0ddd4c4ec
PuzanovAlexander <leifon2011@gmail.com>
2023-02-27 19:37:29 +0300

    изменены названия переменых

--- lab_practic1.py
+++ lab_practic1.py
@@ -1,6 +1,6 @@
-a=input("веедите пароль:")
-b=input("повторите пароль:")
-if a==b:
+password=input("веедите пароль:")
+repassword=input("повторите пароль:")
+if password==repassword:
     print ("Пароль принят")
 else:
     print ("Пароль не принят")
